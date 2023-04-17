57c53fe81f6e1282da0c6d77e1c7b4b4041fa99f
Matyi Kari <matykari@gmail.com>
2023-04-17 14:59:18 -0600

    initial commit

---
+++ src/components/Airbnb/Airbnb.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export interface AirbnbProps {
+  id: string
+  reviews: boolean
+  nofollow: boolean
+}
+
+const Airbnb = ({ id, reviews = true, nofollow = false }: AirbnbProps) => {
+  return (
+    <div
+      className='airbnb-embed-frame'
+      data-id={id}
+      data-view='home'
+      data-hide-reviews={!reviews}
+      style={{ width: 450, height: 300, margin: 'auto' }}>
+      <a
+        href={`https://www.airbnb.com/rooms/${id}?check_in=2023-05-24&amp;check_out=2023-05-30&amp;guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget`}
+        rel={nofollow ? 'nofollow' : ''}>
+        View On Airbnb
+      </a>
+      <a
+        href={`https://www.airbnb.com/rooms/${id}?check_in=2023-05-24&amp;check_out=2023-05-30&amp;guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget`}
+        rel={nofollow ? 'nofollow' : ''}>
+        Invisible House Joshua Tree | Modern Masterpiece
+      </a>
+      <script
+        async
+        src='https://www.airbnb.com/embeddable/airbnb_jssdk'></script>
+    </div>
+  )
+}
+
+export default Airbnb
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as Airbnb } from './Airbnb'
